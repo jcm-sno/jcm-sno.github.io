0a07d17972b4e8b92e4e39b7f784027986e449d4
James <jamescmorrison00@gmail.com>
2026-08-31 19:24:28 -0700

    Add RSVPify embed component

---
+++ app/components/rsvpify-embed.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+
+const RSVPIFY_EMBED_URL = "https://jcm-sno.rsvpify.com/embed";
+
+export default function RsvpifyEmbed() {
+  const hostRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const host = hostRef.current;
+    if (!host) return;
+
+    host.replaceChildren();
+
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = RSVPIFY_EMBED_URL;
+    host.appendChild(script);
+
+    return () => {
+      host.replaceChildren();
+    };
+  }, []);
+
+  return (
+    <div
+      ref={hostRef}
+      className="rsvpify-embed-host"
+      aria-label="Wedding RSVP form"
+    />
+  );
+}
